a1d7098f23c2c1f53bc83352dea9e69a855eb113
Guanghui Liang <meetguanghuiliang@gmail.com>
2024-01-14 01:52:51 +0800

    chore: update Package.swift

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "NetworkingX",
-    platforms: [.macOS(.v10_15), .iOS(.v15)],
+    platforms: [.macOS(.v11), .iOS(.v15)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
@@ -18,7 +18,8 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "NetworkingX"),
+            name: "NetworkingX",
+            dependencies: ["Alamofire", "FoundationX"]),
         .testTarget(
             name: "NetworkingXTests",
             dependencies: ["NetworkingX"]),
