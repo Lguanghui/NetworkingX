e980d4a55cf35c78ae79849e43ad3b82805a236c
Guanghui Liang <meetguanghuiliang@gmail.com>
2024-05-16 11:15:16 +0800

    ✨ Add common http headers for Alamofire

--- Sources/NetworkingX/NetworkConfig.swift
+++ Sources/NetworkingX/NetworkConfig.swift
@@ -8,6 +8,13 @@
 
 import Foundation
 import FoundationX
+import Alamofire
+
+public extension Alamofire.HTTPHeaders {
+    static var commonHeaders: HTTPHeaders {
+        NetworkConfig.shared.commonHeaders
+    }
+}
 
 public final class NetworkConfig: ObservableObject {
     
@@ -26,10 +33,38 @@ public final class NetworkConfig: ObservableObject {
     
     public private(set) var port: Value<Int> = .none
     
+    public private(set) var commonHeaders: HTTPHeaders = .default
+    
     /// update current network config. **It is recommended to call it before rendering the view**
     public func updateConfig(host: String, scheme: Value<String> = .some(value: "https"), port: Value<Int> = .none) {
         self.host = host
         self.scheme = scheme
         self.port = port
+    }
+}
+
+// MARK: - Headers
+
+public extension NetworkConfig {
+    func commonHeadersAppend(_ headers: HTTPHeaders) {
+        headers.forEach { header in
+            commonHeaders.add(header)
+        }
+    }
+    
+    func commonHeadersAppend(_ header: HTTPHeader) {
+        commonHeaders.add(header)
+    }
+    
+    func commonHeadersUpdate(name: String, value: String) {
+        commonHeaders.update(name: name, value: value)
+    }
+    
+    func commonHeadersUpdate(_ header: HTTPHeader) {
+        commonHeaders.update(header)
+    }
+    
+    func commonHeadersRemove(_ name: String) {
+        commonHeaders.remove(name: name)
     }
 }
